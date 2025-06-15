1ea69e936f1246e861aec3fd6b3a17f8238b639a
Rex <rexrex10050@gmail.com>
2025-06-15 17:21:22 +0800

    new

---
+++ src/main/java/com/rex/guessthenumberab/GuessTheNumberAbApplication.java
@@ -0,0 +1,13 @@
+package com.rex.guessthenumberab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessTheNumberAbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuessTheNumberAbApplication.class, args);
+    }
+
+}
